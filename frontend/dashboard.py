b7adfe294d50397efb818fc3c44f8e82f418270a
MattiaDiProfio <mattia.diprofio@gmail.com>
2025-09-12 10:03:04 +0100

    add code to render the data quality reports on the dashboard

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -63,6 +63,40 @@ def main():
                     # render the processed dataframe to screen
                     st.dataframe(processed_dataframe.head())
 
+                    # render the data quality reports for the uploaded dataset and let the user download them
+                    # DQR -> data quality report
+
+                    st.divider()
+                    st.subheader("Data Quality Reports")
+                    st.text("Find below the data quality report computed for your dataset before and after applying the transformations requested. Note that these report are computed with respect to your task requirements")
+
+                    dqr_before = response_body.get('data-quality-before', None)
+                    dqr_after = response_body.get('data-quality-after', None)
+
+                    if dqr_before:
+                        # TODO - might need to render this into an easy-to-read table, can be done later on
+                        st.text("Data Quality Report (pre-transformation)")
+                        st.json(dqr_before)
+                        st.download_button(
+                            label="📥 Download",
+                            data=json.dumps(dqr_before, indent=2),
+                            file_name=f"dqr_before_{dataset_name}.json",
+                            mime="application/json"
+                        )
+
+                    if dqr_after:
+                        # TODO - might need to render this into an easy-to-read table, can be done later on
+                        st.text("Data Quality Report (post-transformation)")
+                        st.json(dqr_after)
+                        st.download_button(
+                            label="📥 Download",
+                            data=json.dumps(dqr_after, indent=2),
+                            file_name=f"dqr_after_{dataset_name}.json",
+                            mime="application/json"
+                        )
+
+                    # TODO - could even include an analysis message that summarises the data quality improvements between the before and after
+
                 else:
                     # FastAPI uses HTTPException by default, hence we assume an error returns the "detail" key
                     st.error(response_body.get("detail"))
